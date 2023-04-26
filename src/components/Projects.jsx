cda930508ad4b907740412e5afd24a5c5a1b3cd0
Anthony Lenglet <anthony.lenglet2@gmail.com>
2023-04-26 16:38:41 +0200

    responsive small desktop

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,30 +12,23 @@ function Projects() {
       <div className="cards-container">
         <div className="card">
           <div className="card__side card__side--front">
-            <img src={booki} alt="Booki website" />
+            <img src={grimoire} alt="Mon Vieux Grimoire website" />
           </div>
           <div className="card__side card__side--back">
-            <h3>Booki</h3>
+            <h3>Mon Vieux Grimoire</h3>
             <div className="card__details">
               <ul>
                 <li>
-                  Création de la page d'accueil d'une agence de voyage avec HTML
-                  et CSS
+                  Développement du <strong>back-end</strong> d'un site de
+                  notation de livres avec{" "}
+                  <strong>Node.js, Express et MongoDB</strong>
                 </li>
-                <li>
-                  Installation de l'environnement de développement front-end
-                </li>
-                <li>
-                  Intégration du contenu conformément à la maquette avec HTML et
-                  CSS
-                </li>
-                <li>
-                  Implémentation de l'interface responsive avec HTML et CSS
-                </li>
-                <li>Versionnage du projet avec Git et Github</li>
+                <li>Implémentation d'un modèle logique de données</li>
+                <li>Stockage des données de manière sécurisée</li>
+                <li>Mise en œuvre des opérations CRUD de manière sécurisée</li>
               </ul>
             </div>
-            <a href="http://localhost:3000/" className="btn">
+            <a href="http://localhost:3000/" className="btn btn--white">
               Voir le site
             </a>
           </div>
@@ -43,19 +36,26 @@ function Projects() {
 
         <div className="card">
           <div className="card__side card__side--front">
-            <img src={sophie} alt="Sophie Bluel website" />
+            <img src={kasa} alt="Kasa website" />
           </div>
           <div className="card__side card__side--back">
-            <h3>Sophie Bluel</h3>
+            <h3>Kasa</h3>
             <div className="card__details">
               <ul>
-                <li>Création d'une page web dynamique avec JavaScript</li>
                 <li>
-                  Récupération des données utilisateurs dans le JavaScript via
-                  des formulaires
+                  Création du <strong>front-end</strong> d'une application web
+                  de location immobilière avec <strong>React</strong>
                 </li>
-                <li>Manipulation des éléments du DOM avec JavaScript</li>
-                <li>Gestion des événements utilisateurs avec JavaScript</li>
+                <li>Initialisation d'une application avec Create React App</li>
+
+                <li>
+                  Configuration de la navigation entre les pages de
+                  l'application avec React Router
+                </li>
+                <li>
+                  Développement des éléments de l'interface d'un site web grâce
+                  à des composants React
+                </li>
               </ul>
             </div>
             <a href="http://localhost:3000/" className="btn">
@@ -86,26 +86,22 @@ function Projects() {
 
         <div className="card">
           <div className="card__side card__side--front">
-            <img src={kasa} alt="Kasa website" />
+            <img src={sophie} alt="Sophie Bluel website" />
           </div>
           <div className="card__side card__side--back">
-            <h3>Kasa</h3>
+            <h3>Sophie Bluel</h3>
             <div className="card__details">
               <ul>
                 <li>
-                  Création d'une application web de location immobilière avec
-                  React
-                </li>
-                <li>Initialisation d'une application avec Create React App</li>
-
-                <li>
-                  Configuration de la navigation entre les pages de
-                  l'application avec React Router
+                  Création d'une page web dynamique avec{" "}
+                  <strong>Vanilla JavaScript</strong>
                 </li>
                 <li>
-                  Développement des éléments de l'interface d'un site web grâce
-                  à des composants React
+                  Récupération des données utilisateurs dans le JavaScript via
+                  des formulaires
                 </li>
+                <li>Manipulation des éléments du DOM</li>
+                <li>Gestion des événements utilisateurs</li>
               </ul>
             </div>
             <a href="http://localhost:3000/" className="btn">
@@ -116,21 +112,25 @@ function Projects() {
 
         <div className="card">
           <div className="card__side card__side--front">
-            <img src={grimoire} alt="Mon Vieux Grimoire website" />
+            <img src={booki} alt="Booki website" />
           </div>
           <div className="card__side card__side--back">
-            <h3>Mon Vieux Grimoire</h3>
+            <h3>Booki</h3>
             <div className="card__details">
               <ul>
                 <li>
-                  Développement du back-end d'un site de notation de livres
+                  Création de la page d'accueil d'une agence de voyage avec{" "}
+                  <strong>HTML</strong> et <strong>CSS</strong>
                 </li>
-                <li>Implémentation d'un modèle logique de données</li>
-                <li>Stockage des données de manière sécurisée</li>
-                <li>Mise en œuvre des opérations CRUD de manière sécurisée</li>
+                <li>
+                  Installation de l'environnement de développement front-end
+                </li>
+                <li>Intégration du contenu conformément à la maquette</li>
+                <li>Implémentation de l'interface responsive</li>
+                <li>Versionnage du projet avec Git et Github</li>
               </ul>
             </div>
-            <a href="http://localhost:3000/" className="btn btn--white">
+            <a href="http://localhost:3000/" className="btn">
               Voir le site
             </a>
           </div>
